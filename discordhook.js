9a701f9de0ab24043cc4fd95a05f6393ed3c4208
Bjornskjald <github@bjorn.ml>
2018-05-21 16:23:18 +0200

    Fixed deprecation warning on setGame

--- discordhook.js
+++ discordhook.js
@@ -19,7 +19,7 @@ client.on('ready', () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`)
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setGame(`on ${client.guilds.size} servers`)
+  client.user.setActivity(`on ${client.guilds.size} servers`)
 })
 
 client.on('message', async message => {
